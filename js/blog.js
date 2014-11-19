73f62ca6c9ba7022c21846385439d3824c304609
Tim Van <hoangvd241@gmail.com>
2014-11-19 23:10:46 +1100

    UI design

---
+++ js/blog.js
@@ -0,0 +1,24 @@
+var blog = (function () {
+	'use strict';
+
+	var
+		configMap = {
+			no_of_entries_per_load : 5,
+
+			blog_model : null
+		};
+
+
+	var initModule = function ( $blog ) {
+
+	};
+
+	var loadMoreEntries = function ( $blog, starting_entry_index ) {
+		
+	};
+
+	return { 
+		initModule : initModule,
+		loadMoreEntries : loadMoreEntries 
+	};
+} ());
