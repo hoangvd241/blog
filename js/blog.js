d6c656e081cd6db00fda708b49b84742e5d656db
Tim Van <hoangvd241@gmail.com>
2014-11-24 23:02:25 +1100

    server code

--- js/blog.js
+++ js/blog.js
@@ -6,15 +6,25 @@ var blog = (function () {
 			no_of_entries_per_load : 5,
 
 			blog_model : null
+		},
+		stateMap = {
+			$append_target : null,
+			earliestEntryPosted : null
 		};
 
 
-	var initModule = function ( $blog ) {
-
+	var initModule = function ( $append_target ) {
+		stateMap.$append_target = $append_target;
+		earliestEntryPosted = new Date();
 	};
 
-	var loadMoreEntries = function ( $blog, starting_entry_index ) {
-		
+	var loadMoreEntries = function () {
+		var entries = configMap.blog_model.entries.load(stateMap.earliestEntryPosted, configMap.no_of_entries_per_load);
+		var i;
+		for (i = 0; i < entries.length; i++) {
+			var html
+		}
+
 	};
 
 	return { 
